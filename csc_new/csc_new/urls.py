aed13b7dde1a4d71f197f31272883f7274dc19bc
Chris <secretevilradio@gmail.com>
2014-07-13 21:50:33 -0400

    fix dakota's thing? sorry bud

--- csc_new/csc_new/urls.py
+++ csc_new/csc_new/urls.py
@@ -3,9 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
+
+
+
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', include(pages.urls)),
+    #url(r'^$', include(pages.urls)),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
