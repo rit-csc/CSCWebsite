2aa9b7b1b88a0f1616819fa3292622b328cd2fda
Dakota Williams <drw9888@rit.edu>
2014-07-13 21:40:50 -0400

    fixes

--- csc_new/csc_new/urls.py
+++ csc_new/csc_new/urls.py
@@ -3,11 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from pages import views
-
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', views.index, name='index'),
+    url(r'^$', include(pages.urls)),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
