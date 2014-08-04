f22f3b2b7d596c61545721920ab967b7798eb5c7
Dakota Williams <drw9888@rit.edu>
2014-08-04 00:12:28 -0400

    POLYMER IS WORKING

--- csc_new/csc_new/urls.py
+++ csc_new/csc_new/urls.py
@@ -1,20 +1,28 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
-
 from pages import views
+from django.conf import settings
 
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+# http://stackoverflow.com/questions/15491727/include-css-and-javascript-in-my-django-template
 
 urlpatterns = patterns('',
     # Examples:
-    #url(r'^$', include(pages.urls)),
+    url(r'^$', RedirectView.as_view(url='index.html', permanent=False)),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^helloWorld', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^helloWorld', include(admin.site.urls)),
+	
+	#Custom stuff GOES HERE
+	
 	url(r'^.*$', views.generic),
+	#Custom stuff DOES NOT GO HERE
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-)
+urlpatterns += staticfiles_urlpatterns()
--- csc_new/pages/views.py
+++ csc_new/pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 
 def generic(request):
-	return HttpResponse("%s" % request.path)
+	template = loader.get_template("pages%s" % request.path)
+	context = RequestContext(request, {
+		#Nothing really should go here
+	})
+	return HttpResponse(template.render(context))
