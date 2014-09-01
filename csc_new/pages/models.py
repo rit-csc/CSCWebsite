aeffb1290f6e44553eff3fdafa91eedef91183e3
Dakota Williams <drw9888@rit.edu>
2014-09-01 02:12:23 -0400

    Stuff: about page, contact page, index now has event feed (untested)

--- csc_new/pages/models.py
+++ csc_new/pages/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+# dependent on icalendar package - pip install icalendar
+from icalendar import Calendar, Event, vDatetime
+from datetime import datetime
+import urllib2
+
 # Create your models here.
+
+class RenderableEvent:
+	def __init__(self, summ, sdate, edate, d):
+		self.summary = summ
+		self.start_date = sdate
+		self.end_date = edate
+		self.desc = d
+
+class RenderableEvents:
+	events = []
+
+	@classmethod
+	def getEvents(self):
+		icalFile = urllib2.urlopen('http://www.google.com/calendar/ical/calendar%40csc.cs.rit.edu/public/basic.ics')
+		ical = Calendar.from_ical(icalFile.read())
+		for thing in ical.walk():
+			eventtime = thing.get('dtstart')
+			if thing.name == "VEVENT" and eventtime.dt.replace(tzinfo=None) > datetime.now():
+				event = RenderableEvent(thing.get('summary'), eventtime.dt.replace(tzinfo=None), thing.get('dtend').dt.replace(tzinfo=None), thing.get('description'))
+				self.events.append(thing)
+		icalFile.close()
--- csc_new/pages/views.py
+++ csc_new/pages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from pages.models import RenderableEvents
+
 # Create your views here.
 
 def generic(request):
@@ -12,6 +14,10 @@ def generic(request):
 	return HttpResponse(template.render(context))
 
 def index(request):
-	return render(request, "pages/index.html")
-
+	RenderableEvents.getEvents()
+	template = loader.get_template("pages%s" % request.path)
+	context = RequestContext(request, {
+		'events' : RenderableEvents.events,
+	})
+	return HttpResponse(template.render(context))
 	
