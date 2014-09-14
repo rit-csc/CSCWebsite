7c610a3b73c89b78da7521640745701e9ffc3ea5
Dakota Williams <drw9888@rit.edu>
2014-09-14 17:28:50 -0400

    Fixed file deletion bug

--- csc_new/pages/models.py
+++ csc_new/pages/models.py
@@ -1,18 +1,26 @@
 from django.db import models
+from django.utils.encoding import force_bytes
 
 # dependent on icalendar package - pip install icalendar
 from icalendar import Calendar, Event, vDatetime
 from datetime import datetime
 import urllib.request, urllib.error, urllib.parse
+import os
+from csc_new import settings
 
 # Create your models here.
-class ExamReview(models.Model):
+class ExamReview(models.Model):	
 	title = models.CharField(max_length=100)
-	questions = models.FileField(upload_to="exam_reviews/")
-	answers = models.FileField(upload_to="exam_reviews/")
-
+	questions = models.FileField(upload_to="exam_reviews")
+	answers = models.FileField(upload_to="exam_reviews")
+		
 	def __str__(self):
-		return self.title;
+		return '%s' % (self.title)
+
+	def delete(self, *args, **kwargs):
+		os.remove(os.path.join(settings.MEDIA_ROOT, str(self.questions)))
+		os.remove(os.path.join(settings.MEDIA_ROOT, str(self.answers)))
+		super(ExamReview, self).delete(*args, **kwargs)
 
 class RenderableEvent:
 	def __init__(self, summ, sdate, edate, d):
