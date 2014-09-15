35284826a014dd855d2bf4838f04914eb3870948
Kaitlin Hipkin <kah5368@rit.edu>
2014-09-14 20:37:38 -0400

    struggling with navbar js stuff

---
+++ csc_new/staticfiles/js/navbar.js
@@ -0,0 +1,22 @@
+function updateNavBar(){
+	var menu = location.pathname;
+	if (menu == "/") {
+    	document.getElementById("index").setAttribute("class", "active");
+	} else if (menu == "/about") {
+    	document.getElementById("about").setAttribute("class", "active");
+	} else if (menu == "/contact") {
+    	document.getElementById("contact").setAttribute("class", "active");
+	} else if (menu == "/projects") {
+    	document.getElementById("projects").setAttribute("class", "active");
+	} else if (menu == "/calendar") {
+    	document.getElementById("calendar").setAttribute("class", "active");
+	} else if (menu == "/resources") {
+    	document.getElementById("resources").setAttribute("class", "active");
+	}
+}
+
+
+
+function setActive(id){
+    document.getElementById(id).setAttribute("class", "active");
+}
