376dc2098c292f9bac3f23c5f97124cbc7da4656
Kaitlin Hipkin <kah5368@rit.edu>
2014-09-18 22:45:01 -0400

    added helper methods for classes in member/models.py (haven't tested them yet)

--- csc_new/member/models.py
+++ csc_new/member/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# TODO: EventResources needs to implemented to replace pages.models.ExamReview
+# TODO: EventResources needs to be implemented to replace pages.models.ExamReview
 
 # MemberType - defines member types
 # 	typeName - the human-readable name of the type
@@ -30,6 +30,19 @@ class Member(models.Model):
 	email = models.EmailField()
 	type = models.ForeignKey(MemberType)
 
+	def committee_memberships(self):
+		return CommitteeMembership.objects.filter(commMember=self.pk)
+
+	def event_logins(self):
+		return EventLogin.objects.filter(attendee=self.pk)
+
+	# TODO: FIGURE OUT HOW TO DO THIS BETTER
+	def events_attended(self):
+		events = []
+		for login in event_logins(self):
+			events.append(Event.objects.get(pk=login.eventNo.pk))
+		return events
+
 # Event - defines an event | TODO: make this work with index.html and Google calendar
 #	eventName - name of the event
 #	eventTime - time of the event
@@ -37,6 +50,9 @@ class Event(models.Model):
 	# id = models.AutoField(primary_key=True) # automatically created as long as no other primary key is defined
 	eventName = models.CharField(max_length=80)
 	eventTime = models.DateTimeField()
+
+	def event_logins(self):
+		return EventLogin.objects.filter(eventNo=self.pk)
 
 # EventLogin - tracks members who sign in at events
 #	eventNo - a foreign key to the event signed into
@@ -55,12 +71,15 @@ class Committee(models.Model):
 	commName = models.CharField(max_length=80)
 	chair = models.ForeignKey(Member)
 	commEmail = models.EmailField()
+
+	def member_list(self):
+		return CommitteeMembership.objects.filter(comm=self.pk)
 	
-# CommitteeMembers - tracks who is members of committees
+# CommitteeMembership - represents a members membership in a committees (many-to-many mapping)
 #	commMember - a foreign key to the member in a committee
 #	commName - a foreign key to committee a member is a part of
-class CommitteeMembers(models.Model):
+class CommitteeMembership(models.Model):
 	# id = models.AutoField(primary_key=True) # automatically created as long as no other primary key is defined
 	commMember = models.ForeignKey(Member)
-	commName = models.ForeignKey(Committee)
+	comm = models.ForeignKey(Committee)
 	
