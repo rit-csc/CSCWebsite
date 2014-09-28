6e5d859b9cab64017328b31bec351bd226d62dfd
Dakota Williams <drw9888@rit.edu>
2014-09-28 12:47:48 -0400

    Carousel actually works

--- csc_new/staticfiles/js/csc-carousel.js
+++ csc_new/staticfiles/js/csc-carousel.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-	var arr = document.getElementsByClassName("carousel-img");
+	var arr = $("#carousel > div");
 	
 	var curr = 0;
 	
-	arr[(arr.length+curr-1) % arr.length].className = "outfocus-left"; //because js does mod SHITTILY
-	arr[curr % arr.length].className = "focus";
-	arr[(curr+1) % arr.length].className = "outfocus-right";
-	
-	curr = 2;
+	$($("#carousel > div").get((arr.length+curr-1) % arr.length)).switchClass("carousel-img", "outfocus-left");
+	$($("#carousel > div").get(curr)).switchClass("carousel-img", "focus");
+	$($("#carousel > div").get((arr.length+curr+1) % arr.length)).switchClass("carousel-img", "outfocus-right");
 	
 	var loopFunc = function() {					
 		//$("#carousel > div").each(function(index, elem) {
-		for(var i = 0; i < $("#carousel > div").length; i++) {
+/*		for(var i = 0; i < $("#carousel > div").length; i++) {
 			switch ($($("#carousel > div").get(i)).attr('class')) {
 				case "focus":
 					$($("#carousel > div").get(i)).switchClass("focus", "outfocus-left");
@@ -28,7 +26,12 @@ $(document).ready(function() {
 					}
 			}
 			
-		}//);
+		}//);*/
+		$($("#carousel > div").get((arr.length+curr-1) % arr.length)).switchClass("outfocus-left", "carousel-img");
+		$($("#carousel > div").get(curr)).switchClass("focus", "outfocus-left");
+		$($("#carousel > div").get((arr.length+curr+1) % arr.length)).switchClass("outfocus-right", "focus");
+		$($("#carousel > div").get((arr.length+curr+2) % arr.length)).switchClass("carousel-img", "outfocus-right");
+		
 		curr++;
 		if(curr >= arr.length) {
 			curr = curr % arr.length
