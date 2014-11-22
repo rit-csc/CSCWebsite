3c1b18aa989661d5517bfd16f706fe4878b57a52
Kaitlin Hipkin <kah5368@rit.edu>
2014-11-22 14:35:12 -0500

    fixed all of those weird timezone issues!

--- csc_new/pages/models.py
+++ csc_new/pages/models.py
@@ -38,7 +38,7 @@ class Photo(models.Model):
 		super(Photo, self).delete(*args, **kwargs)
 
 # RenderableEvent - holds an event
-class RenderableEvent:
+class RenderableEvent(models.Model):
 	__slots__=('summary', 'start_date', 'start_time', 'end_time', 'desc', 'pureTime', 'location')
 
 	def __init__(self, summ, sdate, stime, etime, d, stimePure, loc):
@@ -54,7 +54,7 @@ class RenderableEvent:
 		return self.summary + " " + self.start_date + " "+ self.start_time + " "+ self.end_time + " " + self.location
 
 # RenderableEvents - holds all events
-class RenderableEvents:
+class RenderableEvents(models.Model):
 	__slots__ = ('events')
 	
 	def __init__(self):
@@ -63,34 +63,37 @@ class RenderableEvents:
 	def getEvents(self):
 		icalFile = urllib.request.urlopen('http://www.google.com/calendar/ical/calendar%40csc.cs.rit.edu/public/basic.ics')
 		ical = Calendar.from_ical(icalFile.read())
-	#	lt = LocalTimezone()
-		offset = timedelta(hours=-5)
+
+		lt = LocalTimezone()
+
 		for thing in ical.walk():
+
 			eventtime = thing.get('dtstart')
-			# if eventtime != None:
-			# 	offset = lt.utcoffset(eventtime.dt)
+			if eventtime != None:
+				offset = lt.utcoffset(eventtime.dt)
+
 			loc = thing.get('location')
-			if ( loc== None) or (loc == "") or (loc == "TBD"):
+			if (loc == None) or (loc == "") or (loc == "TBD"):
 				loc = "TBD"
+
 			if thing.name == "VEVENT" and eventtime.dt.replace(tzinfo=None)+offset > datetime.today() - timedelta(days=1):
-				event = RenderableEvent(thing.get('summary'), (eventtime.dt.replace(tzinfo=None)+offset).strftime("%m/%d/%Y"), \
+
+				event = RenderableEvent(
+					thing.get('summary'), \
+					(eventtime.dt.replace(tzinfo=None)+offset).strftime("%m/%d/%Y"), \
 					(eventtime.dt.replace(tzinfo=None)+offset).strftime("%I:%M %p"),\
-					(thing.get('dtend').dt.replace(tzinfo=None)+offset).strftime("%I:%M %p"), thing.get('description'),\
-					(eventtime.dt.replace(tzinfo=None)+offset), loc)
-				inserted = False
-				# TODO this can probably be improved in terms of efficiency.
-				for i in range(len(self.events)): # this appears to orders our events by date! ... backwards.
-					if self.events[i].pureTime < (eventtime.dt.replace(tzinfo=None)+offset):
-						self.events.insert(i,event)
-						inserted = True
-						break
-				if not inserted:
-					self.events.append(event)
+					(thing.get('dtend').dt.replace(tzinfo=None)+offset).strftime("%I:%M %p"),  \
+					thing.get('description'),\
+					(eventtime.dt.replace(tzinfo=None)+offset), \
+					loc)
+
+				self.events.append(event)
+
 			elif thing.name == "VEVENT" and thing.get('RRULE') is not None:
 				repeats = list(rrule.rrulestr(thing.get('RRULE').to_ical().decode('unicode_escape'), ignoretz=True, dtstart=datetime.now()))
 				if(len(repeats) <= 0):
 					continue
-				#Remove after Fall Semester 2014
+				# Remove after Fall Semester 2014!
 				if(thing.get('summary') == 'General Meeting!'):
 					continue
 				# # # # # # # # # # # # # # # # #
@@ -98,6 +101,9 @@ class RenderableEvents:
 					(thing.get('dtstart').dt.replace(tzinfo=None)).strftime("%I:%M %p"), \
 					(thing.get('dtend').dt.replace(tzinfo=None)).strftime("%I:%M %p"), thing.get('description'), \
 					(repeats[0].replace(tzinfo=None)), loc))
-		self.events = self.events[::-1] # reverse the list, because it was backwards by date!
+
+		# Sort events by date and time!
+		self.events = sorted(self.events, key=lambda renderable_event: renderable_event.pureTime)
+
 		icalFile.close()
 
