a8b9274fb8d1545b84bd24bd74fb6ffceaf4cd09
Kaitlin Hipkin <kah5368@rit.edu>
2015-01-07 22:30:49 -0500

    NOTE: ADDS DEPENDENCY ON REQUESTS!!!!!!! replace hard-coded repository info on projects.html with data pulled from the csc's github using the github api

--- csc_new/pages/templatetags/extras.py
+++ csc_new/pages/templatetags/extras.py
@@ -7,27 +7,9 @@ from pages.models import *
 
 register = template.Library()
 
-# Repository attributes that may be useful at some point:
-		# r.readme() -> :class: `Contents <github3.repos.contents.Contents>`
-	#	# r.description -> Description of the repository
-		# r.homepage -> URL of the home page for the project (GH-PAGES)
-	#	# r.html_url -> URL of the project at GitHub (SRC CODE)
-	#	# r.language -> language property
-	#	# r.name -> name of the repository (i.e., "github3.py")
-	#	# r.stargazers -> number of users who starred the repository
-	#	# r.watchers -> number of users watching the repository
-	#	# str(r) -> the full name
 @register.inclusion_tag('pages/inclusionTemplates/repoInfo.html')
 def repoInfo(repo):
-	fullname="rit-csc/%s" %(repo["name"])
-	return {
-			"name":repo["name"],
-			"fullname":"%s" %(fullname),
-			"html_url":"http://github.com/%s" %(fullname),
-			"description":repo["description"],
-			"language":repo["language"],
-			"misc_info":repo["misc_info"]
-			}
+	return repo
 
 @register.filter
 def get_item(dictionary, key):
--- csc_new/pages/views.py
+++ csc_new/pages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+import requests
+
 from pages.models import *
 from csc_new import settings
 
@@ -10,8 +12,8 @@ from csc_new import settings
 def generic(request):
 	template = loader.get_template("pages%s.html" % request.path)
 	context = RequestContext(request, {
-		'MEDIA_URL' : settings.MEDIA_URL,
-		'img_list' : Photo.objects.values_list('src', flat=True),
+		'MEDIA_URL':settings.MEDIA_URL,
+		'img_list':Photo.objects.values_list('src', flat=True),
 	})
 	return HttpResponse(template.render(context))
 
@@ -21,8 +23,8 @@ def index(request):
 	template = loader.get_template("pages/index.html")
 	context = RequestContext(request, {
 		'events' : re.events,
-		'MEDIA_URL' : settings.MEDIA_URL,
-		'img_list' : Photo.objects.values_list('src', flat=True),
+		'MEDIA_URL':settings.MEDIA_URL,
+		'img_list':Photo.objects.values_list('src', flat=True),
 	})
 	return HttpResponse(template.render(context))
 	
@@ -30,8 +32,8 @@ def resources(request):
 	template = loader.get_template("pages/resources.html")
 	context = RequestContext(request, {
 		'exams' : ExamReview.objects.all(),
-		'MEDIA_URL' : settings.MEDIA_URL,
-		'img_list' : Photo.objects.values_list('src', flat=True),
+		'MEDIA_URL':settings.MEDIA_URL,
+		'img_list':Photo.objects.values_list('src', flat=True),
 	})
 	return HttpResponse(template.render(context))
 
@@ -39,45 +41,47 @@ def pictures(request):
 	template = loader.get_template("pages/pictures.html")
 	context = RequestContext(request, {
 		'pics' : Photo.objects.all(),
-		'MEDIA_URL' : settings.MEDIA_URL,
-		'img_list' : Photo.objects.values_list('src', flat=True),
+		'MEDIA_URL':settings.MEDIA_URL,
+		'img_list':Photo.objects.values_list('src', flat=True),
 	})
 	return HttpResponse(template.render(context))
 
 def projects(request):
 	template = "pages/projects.html"
-	repos = {
-				"csc_exam_reviews":{
-					"name":"csc_exam_reviews",
-					"description":"A collection of practice exams for exams in core curriculum CS courses at RIT.",
-					"language":"TeX",
-					"misc_info":"The source files for our department-sponsored exam review sessions, currently including content for all CS1, CS2, and Mechanics of Programming exams."
-				},
-				"git_instructions":{
-					"name":"git_instructions",
-					"description":"An introductory tutorial for setting up and using a Git repository, developed for the CS department by the CSC.",
-					"language":"HTML, CSS",
-					"misc_info":"Our work-in-progress tutorial to help RIT's CS department incorporate Git as the primary type of version control in the introductory course curriculum."
-				},
-				"sensorship":{
-					"name":"sensorship",
-					"description":"A simple Pong desktop game that utilizes the accelerometer data from connected Android devices.",
-					"language":"Java",
-					"misc_info":"This project was a hack for Yale University's Yhack 2013."
-				},
-				"MorseCode":{
-					"name":"MorseCode",
-					"description":"An Android app developed by Mike Lyman and Doug Krofcheck that makes discrete Morse Code texting available to the masses.",
-					"language":"Java",
-					"misc_info":"This project was a hack for Yale University's Yhack 2013."
-				},
-				"CSC-Plays-Pokemon":{
-					"name":"CSC-Plays-Pokemon",
-					"description":"A program to accompany Eric Falkenberg's workshop on the technical side of \"Twitch Plays Pokémon\".",
-					"language":"C++",
-					"misc_info":""
-				},
-			}
 
-	return render_to_response(template, {"success":True, "repos":repos, 'MEDIA_URL' : settings.MEDIA_URL, 'img_list' : Photo.objects.values_list('src', flat=True)})
+	try:
+		api_url = "https://api.github.com/orgs/rit-csc/repos"
+		r = requests.get(api_url)
+
+		# # Response will have the following structure:
+		# [
+		# 	{
+		# 		"name": 		{{name_of_repo}},
+		# 		"full_name":	"rit-csc/"{{name_of_repo}},
+		# 		"private":		true/false,
+		# 		"html_url":		"https://github.com/rit-csc/"{{name_of_repo}},
+		# 		"description":	{{description}},
+		# 		"fork":			true/false,
+		# 		"updated_at":	{{updated_at}},		# format: 2015-01-01T12:12:12Z
+		# 		"stargazers_count":	{{num_stargazers}},
+		# 		"watchers_count":	{{num_watchers}},
+		# 		"language":		{{language}},
+		# 		"has_issues":	true/false,
+		# 		"has_pages":	true/false,
+		# 		"open_issues_count":	{{open_issues_count}},
+		# 		...
+		# 	},
+		# 	...
+		# ]
+
+		# Sort repos by last_updated, with the most recently-updated repos first.
+		repos = sorted(r.json(), key=lambda repo: repo['updated_at'], reverse=True)
+
+		return render_to_response(template, {"success":True, "repos":repos,
+												'MEDIA_URL':settings.MEDIA_URL,
+												'img_list':Photo.objects.values_list('src', flat=True)})
 	
+	except:
+		return render_to_response(template, {"success":False,
+												'MEDIA_URL':settings.MEDIA_URL,
+												'img_list':Photo.objects.values_list('src', flat=True)})
